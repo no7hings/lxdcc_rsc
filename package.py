8e10d7a57b2e4317a07b9001a27ce58d16a990cf
dongchangbao <dongchangbao@papegames.net>
2021-12-13 10:59:59 +0800

    lxgui: modify and add resource

---
+++ package.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+name = 'lxdcc_rsc'
+
+version = '0.0.111'
+
+description = ''
+
+authors = ['']
+
+tools = []
+
+requires = []
+
+
+def commands():
+    # resouces
+    env.LYNXI_RESOURCES.append('{root}/script/python/.resources')
+
+
+timestamp = 1639363837
+
+format_version = 2
