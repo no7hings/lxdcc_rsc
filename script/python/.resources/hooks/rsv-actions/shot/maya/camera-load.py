79bf3adbccc4fa31847a86a1c937489ec0700707
dongchangbao <dongchangbao@papegames.net>
2022-02-25 19:13:01 +0800

    common update

---
+++ script/python/.resources/hooks/rsv-actions/shot/maya/camera-load.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+
+
+def main(session):
+    file_path = session.rsv_unit.get_result(
+        version='latest'
+    )
+    if file_path:
+        import lxmaya.fnc.importers as mya_fnc_importers
+
+        mya_fnc_importers.CameraAbcImporter(file_path, '/cameras').set_run()
+
+
+if __name__ == '__main__':
+    # noinspection PyUnresolvedReferences
+    main(session)
