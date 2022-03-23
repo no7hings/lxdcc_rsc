4be96330302cf215743d4798abf863d634c3a917
dongchangbao <dongchangbao@papegames.net>
2022-03-23 16:30:30 +0800

    common update

--- script/python/.setup/maya/scripts/userSetup.py
+++ script/python/.setup/maya/scripts/userSetup.py
@@ -12,34 +12,46 @@ class Setup(object):
     @classmethod
     def set_menu_setup(cls):
         def fnc_():
-            print 'lx-maya-menu-setup: start'
+            print 'lx-maya-menu setup: is started'
             from lxmaya import ma_setup
             ma_setup.MayaMenuSetup().set_setup()
-            print 'lx-maya-menu-setup: complete'
+            print 'lx-maya-menu setup: is completed'
         # noinspection PyUnresolvedReferences
         from maya import cmds
         cmds.evalDeferred(fnc_)
     @classmethod
     def set_arnold_setup(cls):
         def fnc_():
-            print 'lx-arnold-setup: start'
+            print 'lx-arnold setup: is started'
             from lxarnold import and_setup
             and_setup.MayaSetup().run()
-            print 'lx-arnold-setup: complete'
+            print 'lx-arnold setup: is completed'
+        # noinspection PyUnresolvedReferences
+        from maya import cmds
+        cmds.evalDeferred(fnc_)
+    @classmethod
+    def set_usd_setup(cls):
+        def fnc_():
+            print 'lx-usd setup: is started'
+            from lxusd import usd_setup
+            usd_setup.UsdSetup.set_environs_setup()
+            print 'lx-usd setup: is completed'
         # noinspection PyUnresolvedReferences
         from maya import cmds
         cmds.evalDeferred(fnc_)
     @classmethod
     def set_run(cls, *args, **kwargs):
         print '*'*40
-        print 'lx-maya-setup: start'
+        print 'lx-maya setup: is started'
         if cls.get_is_maya():
             #
             cls.set_arnold_setup()
             #
+            cls.set_usd_setup()
+            #
             cls.set_menu_setup()
             #
-        print 'lx-maya-setup: complete'
+        print 'lx-maya setup: is completed'
         print '*' * 40
 
 
