dc804d8e6325b67cb4c26cc2ce11c3cbffcf5bb9
dongchangbao <dongchangbao@papegames.net>
2022-04-21 20:17:44 +0800

    common update

--- script/python/.resources/option-hooks/rsv-task-methods/shot/tmp-efx-render-create.py
+++ script/python/.resources/option-hooks/rsv-task-methods/shot/tmp-efx-render-create.py
@@ -39,7 +39,19 @@ def main(session):
                 elif cache_workspace == 'output':
                     hook_option_opt.set('cache_use_output_enable', 1)
                 #
-                cmd_pattern = 'rez-env python pg_tools -- python /l/packages/pg/prod/pg_production_lib/9.9.9/lib/production/efx/vendor_collect/send_efx_render.py send_render_job --proj="{project}" --shot="{shot}" --task="{task}" --output={cache_use_output_enable} --layer=1 --upload_sg={user_upload_shotgun_enable} --user="{user}" --frame="{render_frames}"'
+                usd_effect_components = hook_option_opt.get(
+                    'usd_effect_components'
+                )
+                if usd_effect_components:
+                    hook_option_opt.set(
+                        'usd_effect_components', ','.join(usd_effect_components)
+                    )
+                else:
+                    hook_option_opt.set(
+                        'usd_effect_components', ''
+                    )
+                #
+                cmd_pattern = 'rez-env python pg_tools -- python /l/packages/pg/prod/pg_production_lib/9.9.9/lib/production/efx/vendor_collect/send_efx_render.py send_render_job --proj="{project}" --shot="{shot}" --task="{task}" --output={cache_use_output_enable} --layer=1 --upload_sg={user_upload_shotgun_enable} --user="{user}" --frame="{render_frames}" --ele="{usd_effect_components}"'
                 #
                 cmd = cmd_pattern.format(
                     **hook_option_opt.value
