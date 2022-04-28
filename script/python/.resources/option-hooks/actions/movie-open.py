1ea91b612d29ae019b978f71ec995746e99ca064
dongchangbao <dongchangbao@papegames.net>
2022-04-28 17:08:38 +0800

    common update

--- script/python/.resources/option-hooks/actions/movie-open.py
+++ script/python/.resources/option-hooks/actions/movie-open.py
@@ -2,14 +2,22 @@
 
 
 def main(session):
+    from lxbasic import bsc_core
+
     from lxutil import utl_core
+
     hook_option_opt = session.option_opt
 
     file_path = hook_option_opt.get('file')
     if file_path:
-        utl_core.RvLauncher().set_file_open(
-            file_path
-        )
+        if bsc_core.SystemMtd.get_is_linux():
+            utl_core.RvLauncher().set_file_open(
+                file_path
+            )
+        else:
+            bsc_core.SubProcessMtd.set_run_with_result(
+                'pgrv "{}"'.format(file_path)
+            )
 
 
 if __name__ == '__main__':
